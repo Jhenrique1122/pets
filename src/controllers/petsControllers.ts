26b6ee6af2b69eb4a44a0a750724ba1a76fd12f0
jhrodrigues <jhenriquerodrigues11@gmail.com>
2024-10-11 20:55:46 -0300

    Adicionou servicos, controles, modelos e rotas para pets

---
+++ src/controllers/petsControllers.ts
@@ -0,0 +1,26 @@
+import {Request, Response} from "express";
+import { PetsService } from "../services/petsService";
+
+const service = new PetsService();
+
+export const listPets = async (req: Request, res: Response) => { 
+  res.status(200).json(await service.fetchPets())
+};
+
+export const createPet = async (req: Request, res: Response) => { 
+  const result = await service.createPet(req.body)
+  res.status(201).json(result)
+};
+
+export const getPetById = (req: Request, res: Response) => {
+  const petId = parseInt(req.params.petId)
+
+ };
+
+export const updatePetById = (req: Request, res: Response) => { 
+  console.log("update pet by id")
+};
+
+export const deletePetById = (req: Request, res: Response) => { 
+  console.log("delete pet by id")
+};
---
+++ src/routes/petsRoutes.ts
@@ -0,0 +1,12 @@
+import {Router} from "express";
+import { createPet, deletePetById, getPetById, listPets, updatePetById } from "../controllers/petsControllers";
+
+
+export const petsRouter= Router()
+petsRouter.get('/pets', listPets)
+petsRouter.post('/pets', createPet)
+petsRouter.delete('/pets/:petId(\\d+)', deletePetById)
+petsRouter.put('/pets/:petId(\\d+)', updatePetById)
+petsRouter.get('/pets/:petId(\\d+)', getPetById)
+
+export default petsRouter;
