0b93b086f3dbc39599ff57fb0b6da261e46f995f
jhrodrigues <jhenriquerodrigues11@gmail.com>
2024-10-15 20:55:45 -0300

    Aula4 - Implementação Docker e ADminer

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import petsRouter from "./routes/petsRoutes";
+import {errorHandler} from "./middlewares/errorMidleware";
 
 const app = express();
 const port = 3000;
