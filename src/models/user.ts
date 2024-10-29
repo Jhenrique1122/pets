b42bc9b38b4ebd6d6a8d0d73df48fff77cbf63e4
jhrodrigues <jhenriquerodrigues11@gmail.com>
2024-10-29 20:55:36 -0300

    Versão Final - autenticação do usuario

---
+++ src/models/user.ts
@@ -0,0 +1,55 @@
+import { DataTypes, Model } from "sequelize";
+import sequelize from "../config/database";
+import bcrypt from "bcrypt";
+
+
+class User extends Model {
+  declare id: number;
+  declare name: string;
+  declare email: string;
+  declare password: string;
+}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING(50),
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING(50),
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING(250),
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    tableName: "users",
+    hooks: {
+      beforeCreate: async (user: User) => {
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(user.password, salt)
+      },
+      beforeUpdate: async (user: User) => {
+        if (user.changed("password")) {
+          const salt = await bcrypt.genSalt(10);
+          user.password = await bcrypt.hash(user.password, salt);
+        }
+      },
+    },
+  },
+);
+
+export default User;
